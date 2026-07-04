2acfae8baf9339507c2881fb5135058404ba0820
Ejup <Ejup-A@users.noreply.github.com>
2026-07-04 21:11:32 +0200

    introduce macro-based DSL with compile-time rule accumulation and dynamic execution

--- discount.exs
+++ discount.exs
@@ -3,17 +3,32 @@ defmodule Discount do
     quote do
       import unquote(__MODULE__)
       Module.register_attribute(__MODULE__, :discounts, accumulate: true)
-
-      def apply_discount do
-        IO.puts "Applying the discounts (#{inspect @discounts})"
-      end
+      @before_compile unquote(__MODULE__)
 
     end
   end
 
-  defmacro discount(name, condition, action) do
+  defmacro __before_compile__(_env) do
     quote do
-      @discounts {unquote(name), unquote(condition), unquote(action)}
+      def apply_discount(product) do
+        Enum.reduce(@discounts, product, fn discount, acc ->
+          apply_discount_rule(acc, discount)
+        end)
+    end
+
+    defp apply_discount_rule(product, {_name, condition_func, action_func}) do
+      if apply(__MODULE__, condition_func, [product]) do
+        apply(__MODULE__, action_func, [product])
+      else
+        product
+      end
+    end
+  end
+end
+
+  defmacro discount(name, condition, action) do
+    quote bind_quoted: [name: name, condition: condition, action: action] do
+      @discounts {name, condition, action}
     end
   end
 end
@@ -22,7 +37,14 @@ defmodule Discounts do
   use Discount
 
   discount :over_100, :is_over_100?, :apply_10_percent_discount
-
   def is_over_100?(product), do: product.price > 100
   def apply_10_percent_discount(product), do: Map.update!(product, :price, &(&1 * 0.9))
+
+  discount :electronics, :is_electronics?, :apply_5_percent_discount
+  def is_electronics?(product), do: product.category == "Electronics"
+  def apply_5_percent_discount(product), do: Map.update!(product, :price, &(&1 * 0.95))
+
+  discount :free_shipping, :is_eligible_for_free_shipping?, :apply_free_shipping
+  def is_eligible_for_free_shipping?(product), do: product.price > 50
+  def apply_free_shipping(product), do: Map.put(product, :free_shipping, true)
 end
