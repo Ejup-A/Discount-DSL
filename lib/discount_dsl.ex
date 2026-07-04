3236b4af6b6915ffc6e19c4b9758da663ab47b3a
Ejup <Ejup-A@users.noreply.github.com>
2026-07-04 21:45:57 +0200

    added unit testing

--- lib/discount_dsl.ex
+++ lib/discount_dsl.ex
@@ -1,3 +1,34 @@
 defmodule DiscountDsl do
-  
+  defmacro __using__(_options) do
+    quote do
+      import unquote(__MODULE__)
+      Module.register_attribute(__MODULE__, :discounts, accumulate: true)
+      @before_compile unquote(__MODULE__)
+
+    end
+  end
+
+  defmacro __before_compile__(_env) do
+    quote do
+      def apply_discount(product) do
+        Enum.reduce(@discounts, product, fn discount, acc ->
+          apply_discount_rule(acc, discount)
+        end)
+    end
+
+    defp apply_discount_rule(product, {_name, condition_func, action_func}) do
+      if apply(__MODULE__, condition_func, [product]) do
+        apply(__MODULE__, action_func, [product])
+      else
+        product
+      end
+    end
+  end
+end
+
+  defmacro discount(name, condition, action) do
+    quote bind_quoted: [name: name, condition: condition, action: action] do
+      @discounts {name, condition, action}
+    end
+  end
 end
--- test/discount_dsl_test.exs
+++ test/discount_dsl_test.exs
@@ -1,8 +1,53 @@
 defmodule DiscountDslTest do
   use ExUnit.Case
-  doctest DiscountDsl
 
-  test "greets the world" do
-    assert DiscountDsl.hello() == :world
+  test "Ensure the Module Loads" do
+    assert Code.ensure_loaded!(DiscountDsl)
+  end
+
+  #1. Apply a discount to a product if the condition is met.
+  test "single discount is applied correctly" do
+    product = %{price: 60, category: "Toys"}
+    discounted_product = Discounts.apply_discount(product)
+
+    assert(discounted_product.free_shipping)
+    assert discounted_product.price == 60
+    assert discounted_product.category == "Toys"
+  end
+
+  #2. Apply multiple discounts in sequence if multiple conditionsare met.
+  test "multiple discounts are applied in order" do
+    product = %{price: 200, category: "Electronics"}
+    discounted_product = Discounts.apply_discount(product)
+
+    assert(discounted_product.free_shipping)
+    assert discounted_product.price == 171
+    assert discounted_product.category == "Electronics"
+  end
+
+  #3. Ensure discounts are not applied if conditions are not met.
+  test "No Discounts applied if conditions are not met" do
+    product = %{price: 40, category: "Toys"}
+    discounted_product = Discounts.apply_discount(product)
+
+    assert discounted_product == product
+  end
+
+  #4. Handle edge cases, if a product is $100.
+  test "Checking edge case to make sure no discount is applied if product is 100$" do
+  product = %{price: 100, category: "Toys"}
+  discounted_product = Discounts.apply_discount(product)
+
+  assert discounted_product.price == 100
+  assert(discounted_product.free_shipping)
+  assert discounted_product.category == "Toys"
+
+  end
+  #5. Invalid inputs are handled gracefully.
+  test "invalid product: missing price" do
+  product = %{category: "Toys"}
+  discounted_product = Discounts.apply_discount(product)
+
+  assert discounted_product == product
   end
 end
